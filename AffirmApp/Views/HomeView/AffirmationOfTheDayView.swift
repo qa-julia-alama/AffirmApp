3465b334ff668df263d1c930d445a8446c7f341a
Julia Alama <qa.julia.alama@gmail.com>
2023-12-29 22:56:21 +0100

    fix text alignment and set minimum deployment target to iOS 16.0

--- AffirmApp/Views/HomeView/AffirmationOfTheDayView.swift
+++ AffirmApp/Views/HomeView/AffirmationOfTheDayView.swift
@@ -11,15 +11,20 @@ struct AffirmationOfTheDayView: View {
     @Binding var affirmation: Affirmation
     var body: some View {
         
-        ZStack {
+// ZStack {
             RoundedRectangle(cornerRadius: 30)
                 .fill(Constans.gradient)
                 .frame(height: 200)
                 .shadow(radius: 20)
                 .padding()
+                .overlay(alignment: .center) {
+                    Text(affirmation.description)
+                        .padding(.horizontal, 15)
+                        .multilineTextAlignment(.center)
+                }
             
-            Text(affirmation.description)
-        }//ZStack
+          
+       // }//ZStack
         
     }
 }
