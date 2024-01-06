2dee1d3ce58b20ecabf7178a47fab6f69a2f02cc
Julia Alama <qa.julia.alama@gmail.com>
2024-01-06 18:22:20 +0100

    Add picker

--- AffirmApp/Views/HomeView/AffirmationView.swift
+++ AffirmApp/Views/HomeView/AffirmationView.swift
@@ -8,52 +8,71 @@
 import SwiftUI
 import CoreData
 
+enum PickerTab:  StringLiteralType, CaseIterable {
+    case moje
+    case ulubione
+}
+
 struct AffirmationView: View {
  
     @StateObject var viewModel = AffirmationViewModel()
     @State var textFieldText: String = ""
     @State private var isAddAffirmationViewPresented: Bool = false
+    @State private var pickerTab: PickerTab = .moje
     
     var body: some View {
         NavigationView {
+            
             VStack(spacing: 20) {
-                
-                List {
-                    ForEach(viewModel.savedEntities, id: \.self) { affirmation in
-                        Text(affirmation.name ?? "NO NAME")
-                    }
-                    .onDelete { indexSet in
-                        viewModel.deleteAffirmation(offsets: indexSet)
-                    }
-                    } // List
-                .navigationTitle("Lista afirmacji")
-                .sheet(isPresented: $isAddAffirmationViewPresented, onDismiss: {
-                    viewModel.fetchAffirmations()
-                }, content: {
-                    if #available(iOS 16.0, *) {
-                        AddAffirmationView()
-                            .presentationDetents([.medium])
-                    } else {
-                        AddAffirmationView()
-                    }
-                })
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button(action: {
-                            // Add affirmation
-                            isAddAffirmationViewPresented.toggle()
-                            
-                        }, label: {
-                            Image(systemName: "plus")
-                        })
-                    }
-                    
-                } //toolbar
+                Picker("", selection: $pickerTab) {
+                    ForEach(PickerTab.allCases, id: \.self) { tab in
+                                Text(tab.rawValue)
+                              }
                 }
-                .listStyle(PlainListStyle())
+                .pickerStyle(.segmented)
                 
+                switch pickerTab {
+                case .moje:
+                    List {
+                        ForEach(viewModel.savedEntities, id: \.self) { affirmation in
+                            Text(affirmation.name ?? "NO NAME")
+                        }
+                        .onDelete { indexSet in
+                            viewModel.deleteAffirmation(offsets: indexSet)
+                        }
+                        } // List
+                    
+                    .sheet(isPresented: $isAddAffirmationViewPresented, onDismiss: {
+                        viewModel.fetchAffirmations()
+                    }, content: {
+                        if #available(iOS 16.0, *) {
+                            AddAffirmationView()
+                                .presentationDetents([.medium])
+                        } else {
+                            AddAffirmationView()
+                        }
+                    })
+                case .ulubione:
+                Text("Tu będą ulubione")
+                }
+                    Spacer()
+                } // VStack
+            .navigationTitle("Lista afirmacji")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        // Add affirmation
+                        isAddAffirmationViewPresented.toggle()
+                        
+                    }, label: {
+                        Image(systemName: "plus")
+                    })
+                }
+                
+            } //toolbar
+            
             }
-            .navigationTitle("Affirmations")
+           
         }
         
     }
